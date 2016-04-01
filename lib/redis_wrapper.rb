0353e6ec71ed7bd0a728bc291204ba10119c7881
Robert Hromej <robert.hromej@gmail.com>
2016-04-01 13:10:46 +0300

    redis_wrapper.rb

--- lib/redis_wrapper.rb
+++ lib/redis_wrapper.rb
@@ -1,20 +1,12 @@
-require "redis_wrapper/version"
-require "bundler"
+require 'redis_wrapper/version'
+require 'bundler'
 Bundler.require
-require "rediscluster"
+require 'rediscluster'
 
 module RedisWrapper
-  # Backward compability with redis-rb
   module RedisClusterFix
     class RedisClient
       def reconnect
-        # reconnect use in /config/unicorn.rb
-        # after_fork do |server, worker|
-        #   Redis.current.client.reconnect
-        # TODO: need to implement this with RedisCluster ? 
-
-        @client = nil
-        cluster_connection
       end
     end
 
@@ -24,26 +16,21 @@ module RedisWrapper
   end
 
   class Wrapper
-    attr_accessor :redis, :is_cluster, :cluster_nodes, :options
+    attr_accessor :is_cluster, :cluster_nodes
 
-    def initialize(options = {})
-      @options = options.dup
-
-      if options[:is_redis_cluster] == true
-        cluster_connection
-      else
-        @is_cluster = false
-        @redis = Redis.new(options)
-      end
+    def self.load(options = {})
+      @@options = options.dup
     end
 
-    def cluster_connection
-      options[:redis_cluster_nodes].each { |x| x.symbolize_keys! }
-      cluster = RedisCluster.new(options[:redis_cluster_nodes])
+    def self.redis
+      @@options[:is_redis_cluster] ? cluster_connection : Redis.new(@@options)
+    end
+
+    def self.cluster_connection
+      @@options[:redis_cluster_nodes].each { |x| x.symbolize_keys! }
+      cluster = RedisCluster.new(@@options[:redis_cluster_nodes])
       cluster.extend RedisClusterFix
-      @is_cluster = true
-      @cluster_nodes = options[:redis_cluster_nodes]
-      @redis = cluster
+      cluster
     end
   end
 end
